fc00aec0267947a71200814e1415b6d7ef354266
Syed Aqib Rehan <syedaqib482002@gmail.com>
2024-10-21 00:37:36 -0700

    UI has completed for all pages like Login page ,Admin Dashboard, Employee dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Login from './components/Auth/Login'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
    {/* <Login/> */}
-   <EmployeeDashboard/>
+   {/* <EmployeeDashboard/> */}
+   <AdminDashboard/>
     </>
   )
 }
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
 import TaskNumberList from '../others/TaskNumberList'
+import TaskList from '../Tasklist/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 text-white h-screen'>
     <Header/>
     <TaskNumberList/>
+    <TaskList/>
     </div>
   )
 }
